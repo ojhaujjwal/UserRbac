6af4ba96afd1953aa9acb795c6713701f63e4f17
Ujjwal Ojha <bring2uo@gmail.com>
2014-04-05 11:55:13 +0545

    Tests passing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,5 +10,5 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
-$loader->addPsr4('UserRbac', __DIR__ . '/../src');
+$loader->addPsr4('UserRbac\\', __DIR__ . '/../src');
 $loader->add('UserRbacTest', __DIR__);
